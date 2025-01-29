c8c1cdcf0d726dcfe428970c3480969e77154b96
Sikirou Bassirou <bassirousikirou59@gmail.com>
2025-01-29 21:31:55 +0100

    integration de paypal Ok

--- src/ui/designSystem/spinner/spinner-screen.tsx
+++ src/ui/designSystem/spinner/spinner-screen.tsx
@@ -3,7 +3,7 @@ import Spinner from './spinner'
 
 export default function SpinnerScreen() {
   return (
-    <div className='flex items-center justify-center h-full '>
+    <div className="flex items-center justify-center min-h-screen w-full">
       <Spinner size='large'/>
     </div>
   )
